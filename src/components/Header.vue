76944ef8cde497bec77349958b0c046a48654394
Erin Hall <halle@mitre.org>
2025-06-25 19:59:39 -0400

    Add header with navigation links

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app-bar app color="primary" dark>
+        <v-toolbar-title>Erin Hall</v-toolbar-title>
+        <v-spacer />
+        <v-btn text to="#about">About</v-btn>
+        <v-btn text to="#projects">Projects</v-btn>
+        <v-btn text to="#resume">Resume</v-btn>
+        <v-btn text to="#contact">Contact</v-btn>
+    </v-app-bar>
+</template>
