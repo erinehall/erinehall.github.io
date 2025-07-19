893dc1a84e517f60b0d8653e45d302633e5b2c6f
erinehall <erinehall@vt.edu>
2025-07-19 15:18:21 -0400

    broke up styles into smaller components and switched to scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import '@mdi/font/css/materialdesignicons.css';
-import './style.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "@mdi/font/css/materialdesignicons.css";
+import "./styles/main.scss";
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).mount("#app");
