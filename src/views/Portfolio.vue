dd054eb7bff012e5e9c265ba8be89220ff517b2e
erinehall <erinehall@vt.edu>
2025-08-01 00:17:16 -0400

    portfolio page

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-container class="py-8" max-width="960">
-    <h1>Portfolio</h1>
+  <v-container class="portfolio-page py-8" max-width="960">
+    <h1 class="text-center mb-6">Portfolio</h1>
+    <v-row>
+      <v-col v-for="(item, i) in portfolio" :key="i" cols="12" md="6">
+        <PortfolioCard v-bind="item" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+
+<script setup>
+import PortfolioCard from "../components/portfolio/PortfolioCard.vue";
+import portfolio from "../data/portfolioData";
+</script>
