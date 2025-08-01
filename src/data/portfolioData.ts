a0d3f5f04a866591de3654d361c4ceea5617bed7
erinehall <erinehall@vt.edu>
2025-08-01 00:16:08 -0400

    data for portfolio cards

---
+++ src/data/portfolioData.ts
@@ -0,0 +1,47 @@
+import virginia_tech from "../assets/virginia-tech.png";
+import aied from "../assets/aied-2023.png";
+import hcii from "../assets/hcii-2024.png";
+
+export default [
+  {
+    title: "Master's Thesis",
+    subtitle:
+      "A User-Centered Design Approach to Evaluating the Usability of Automated Essay Scoring Systems",
+    description:
+      "This thesis presents a qualitative study on the usability of AI-based essay grading tools, focusing on explainability and transparency in feedback for students and educators.",
+    image: virginia_tech,
+    links: [
+      {
+        label: "View Thesis (PDF)",
+        url: "https://vtechworks.lib.vt.edu/server/api/core/bitstreams/12ad03cd-5041-446e-aee9-097ad1d6c617/content",
+      },
+    ],
+  },
+  {
+    title: "Identifying Usability Challenges in AI-Based Essay Grading Tools",
+    subtitle: "AIED 2023 Conference Paper",
+    description:
+      "Explores usability concerns related to AI explainability and transparency in educational platforms, based on a study of student and instructor interactions with an AES tool.",
+    image: aied,
+    links: [
+      {
+        label: "View DOI",
+        url: "https://doi.org/10.1007/978-3-031-36336-8_104",
+      },
+    ],
+  },
+  {
+    title:
+      "Exploring Explainability and Transparency in Automated Essay Scoring Systems",
+    subtitle: "HCII 2024 Conference Paper",
+    description:
+      "Presents findings from a user-centered evaluation of an AI grading platform, identifying key design considerations to enhance trust, clarity, and usability in feedback systems.",
+    image: hcii,
+    links: [
+      {
+        label: "View DOI",
+        url: "https://doi.org/10.1007/978-3-031-61691-4_18",
+      },
+    ],
+  },
+];
