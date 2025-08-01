1d4c32a19cf6f1a971fe67658c56d7542e39a3ad
erinehall <erinehall@vt.edu>
2025-08-01 10:32:46 -0400

    added portfolio preview in about page

---
+++ src/components/PortfolioPreview.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container class="portfolio-preview py-8 mb-4" max-width="960">
+    <h2 class="text-center mb-6">Recent Work</h2>
+    <v-row class="justify-center">
+      <v-col
+        v-for="(item, i) in items"
+        :key="i"
+        cols="12"
+        md="4"
+        class="d-flex justify-center"
+      >
+        <PortfolioCard class="compact-card" v-bind="item" />
+      </v-col>
+    </v-row>
+    <div class="d-flex justify-center mt-6">
+      <v-btn color="primary" @click="goToPortfolio">See More</v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script setup>
+import PortfolioCard from "./portfolio/PortfolioCard.vue";
+import portfolio from "../data/portfolioData";
+import { navigate } from "../router";
+
+const items = portfolio.slice(0, 3);
+
+function goToPortfolio() {
+  navigate("/portfolio");
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+</script>
+
+<style scoped lang="scss">
+.compact-card {
+  max-width: 300px;
+}
+
+.compact-card .v-img {
+  height: 150px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
   <HeroLanding />
+  <PortfolioPreview />
 </template>
 
 <script setup>
 import HeroLanding from "../components/HeroLanding.vue";
+import PortfolioPreview from "../components/PortfolioPreview.vue";
 </script>
