2682144676c69f8d8d14da35e94a1d5e0260ff52
erinehall <erinehall@vt.edu>
2025-08-03 12:44:13 -0400

    added fade in transition to second hero section on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,35 @@
 <template>
   <HeroLanding />
-  <PortfolioPreview />
+  <div
+    ref="secondHeroRef"
+    class="hero-hidden"
+    :class="{ visible: isSecondHeroVisible }"
+  >
+    <PortfolioPreview />
+  </div>
 </template>
 
 <script setup>
 import HeroLanding from "../components/HeroLanding.vue";
+import { ref, onMounted } from "vue";
 import PortfolioPreview from "../components/PortfolioPreview.vue";
+
+const secondHeroRef = ref(null);
+const isSecondHeroVisible = ref(false);
+
+onMounted(() => {
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        isSecondHeroVisible.value = true;
+        observer.unobserve(entries[0].target);
+      }
+    },
+    { threshold: 0.2 }
+  );
+
+  if (secondHeroRef.value) {
+    observer.observe(secondHeroRef.value);
+  }
+});
 </script>
