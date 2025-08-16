6f48ac98b0b624777d4bf2d48e606d22d4cadf5b
erinehall <erinehall@vt.edu>
2025-08-16 00:22:32 -0400

    centered resume section text

--- src/components/resume/ResumeSection.vue
+++ src/components/resume/ResumeSection.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="resume-section pa-0">
-    <h2 class="mb-2">{{ title }}</h2>
+    <h2 class="mb-8 text-center">{{ title }}</h2>
     <slot />
   </v-container>
 </template>
