35f55d46c9b036a61177df565ff214ef51e653b7
Erin Hall <erinhall@Erins-MacBook-Air.local>
2026-02-23 20:12:22 -0500

    Removed unnecessary Vue components and files

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import "@mdi/font/css/materialdesignicons.css";
-import "./styles/main.scss";
-
-createApp(App).use(vuetify).mount("#app");
