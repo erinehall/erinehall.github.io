9c820907a3018640ee200ac89036a59a5d5ace58
Erin Hall <erinhall@Erins-MacBook-Air.local>
2026-02-23 20:19:11 -0500

    Moved components and data folders into src

--- data/contact.ts
+++
@@ -1,8 +0,0 @@
-
-export const contactData = {
-  "email": "erinehall1@gmail.com",
-  "linkedin": "https://linkedin.com/in/erinehall1",
-  "github": "https://github.com/erinehall",
-  "location": "Washington D.C. Metro Area",
-  "profileImage": "public/profile.jpg"
-};
